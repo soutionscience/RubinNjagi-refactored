38f5b0ab441e62d17c2f1bc36d4b9919e0fbcd99
Rubin <marshnjagi@yahoo.com>
2018-01-07 14:57:03 +0300

    added main menu

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,24 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatButton, MatButtonModule, MatIconModule, MatToolbarModule} from '@angular/material';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MyMenuComponent } from './my-menu/my-menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    MyMenuComponent
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatIconModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/server/config.js
@@ -0,0 +1,4 @@
+module.exports={
+    // mongoDbUrl: "mongodb://marsh:chelski20k@ds123956.mlab.com:23956/heroku_c690f4jt",
+    mongoDbUrl: "mongodb://admin:chelski36@ds245357.mlab.com:45357/rubinnjagi"
+}
